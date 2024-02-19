5c4c35538dc8d32da681c46f600e2c665089985d
Ivan <ivanbolbin@gmail.com>
2024-02-19 21:25:04 +0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 #include <math.h>
 
-using namespace std;
-
+/**
+ * @brief принимает вещественное число и возвращает его ближайшее большее чётное число
+ * 
+ * @param n входное число 
+ * @return ближаёщее большее чётное число 
+ */
 int fun1(double n) {
     int c = ceil(n);
     int res_up = c + c * (c % 2) % 2;
     return res_up;
 }
+
+/**
+ * @brief принимает вещественное число и возвращает его ближайшее меньшее чётное число
+ * 
+ * @param n входное число 
+ * @return ближаёщее меньшее чётное число
+ */
 int fun2(double n) {
     int f = floor(n);
     int res_but = f - f * (f % 2) % 2;
     return res_but;
 }
-void fun3(int& n) {
-    int i_1 = n / 100000;
-    int i_2 = n / 10000 % 10;
-    int i_3 = n / 1000 % 10;
-    int i_4 = n / 100 % 10;
-    int i_5 = n / 10 % 10;
-    int i_6 = n % 10;
-    int s_1 = i_1 + i_2 + i_3;
-    int s_2 = i_4 + i_5 + i_6;
-    n = s_1 - s_2;
-}
+
+/**
+ * @brief принимает два аргумента для математической функции и возврощает её результат
+ * 
+ * @param a первый аргумент 
+ * @param b второй аргумент 
+ * @return результат  
+ */
 double fun4(double *a, double *b){
     double x = *a;
     double y = *b;
@@ -40,41 +48,54 @@ double fun4(double *a, double *b){
     return res;
 }
 
-int main() {
-    /*
-    double n;   //создаю вводную переменную
+/**
+ * @brief принимает вещественное число и ваводит его меньшее и большее ближайщее чётное число 
+ * 
+ * @param n входное число 
+ */
+void exercise_1(double n){
     int res_up, res_but, r;    //создаю переменные для результатов и округленного числа
-    cout << "Enter a real number" << endl;
-    cin >> n;   //ввод переменной
+    std::cout << "Enter a real number" << std::endl;
+    //std::cin >> n;   //ввод переменной
     res_up = fun1(n);
     res_but = fun2(n);
-    cout << "Number " << n << " is bounded from below by even number " << res_but   //вывод
-        << " and bounded from above by even number " << res_up << endl;
-    return 0;
-    //*/
+    std::cout << "Number " << n << " is bounded from below by even number " << res_but   //вывод
+        << " and bounded from above by even number " << res_up << std::endl;
+}
 
-    // ТПУ. Информатика 1.2. 
-    // Лабораторная работа №1. Болбин И.М. 
-    // Вариант 2, задание 2.
-    // Дан номер билета ? последовательность из 6 цифр, которая представлена целым
-    // числом.Если в числе меньше 6 цифр, то слева дописываются незначащие нули.
-    // Ввести с клавиатуры целое число и определить, будет ли соответствующий номер
-    // билета «счастливым»(сумма трёх первых цифр равна сумме трёх последних).Если
-    // да, вывести 0, иначе ? любое число, не равное нулю.
-    /*
-    int n;
-    cin >> n;	
-    fun3(n);
-    cout << n;
-    return 0;
-    //*/
+/**
+ * @brief принимает целое число и выводит разность суммы первых трёх и последних трёх цифр числа
+ * 
+ * @param n входное число 
+ */
+void exercise_2(int n){
+    int i_1 = n / 100000;
+    int i_2 = n / 10000 % 10;
+    int i_3 = n / 1000 % 10;
+    int i_4 = n / 100 % 10;
+    int i_5 = n / 10 % 10;
+    int i_6 = n % 10;
+    int s_1 = i_1 + i_2 + i_3;
+    int s_2 = i_4 + i_5 + i_6;
+    n = s_1 - s_2;
+    std::cout << n << std::endl;
+}
 
-    //*
-    double x, y, res;
-    cin >> x; //ввод х
-    cin >> y; //ввод у
+/**
+ * @brief принимает два аргумента для математической функции и возврощает её результат
+ * 
+ * @param x первый аргумент
+ * @param y второй аргумент
+ */
+void exercise_3(double x, double y){
+    double res;
     res = fun4(&x, &y);
-    cout << res << endl;
+    std::cout << res << std::endl;
+}
+
+int main() {
+    exercise_1(2.5);
+    exercise_2(123123);
+    exercise_3(2.5, 3.5);
     return 0;
-    //*/
 }
